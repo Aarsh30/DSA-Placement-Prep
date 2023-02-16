1e790abd93eb0269cb16bcc9edb06595a1ab9e2f
Aarsh30 <patelaarsh3011@gmail.com>
2023-02-16 19:03:56 +0530

    new changes

---
+++ MajorityElement.java
@@ -0,0 +1,20 @@
+class MajorityElement {
+    public int majorityElement(int[] nums) {
+        // Moore's Voting Algorithm
+        int count=0;
+        int candidate=0;
+
+        for(int  num : nums)
+        {
+            if(count == 0)
+            {
+                candidate = num;
+
+            }
+            if(num==candidate) count+=1;
+            else count -=1;
+
+        }
+        return candidate;
+    }
+}
