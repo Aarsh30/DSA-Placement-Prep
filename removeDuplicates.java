eec297cb814e1838c18b4e8ca26663534be28bad
Aarsh30 <patelaarsh3011@gmail.com>
2023-02-18 23:51:15 +0530

    new changes

---
+++ removeDuplicates.java
@@ -0,0 +1,16 @@
+class removeDuplicates {
+    public int removeDuplicates(int[] nums) {
+        
+        int i=0;
+        for(int j=0;j<nums.length;j++)
+        {
+
+            if(nums[i]!=nums[j])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
