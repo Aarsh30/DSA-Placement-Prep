642f2d028185d2e6607ccff270ee4ef700677ec4
Aarsh30 <patelaarsh3011@gmail.com>
2023-03-06 12:51:12 +0530

    new change

---
+++ findKthPositive.java
@@ -0,0 +1,21 @@
+class findKthPositive {
+    public int findKthPositive(int[] arr, int k) {
+        
+    int n=arr.length;
+    int start=0;
+    int end =n-1;
+
+    while(start<=end)
+    {
+     int mid=start+(end-start)/2;
+        if(arr[mid]-(mid+1)<k)
+        start=mid+1;
+        else
+        end= mid-1;
+
+
+    }
+
+return start+k;
+    }
+}
