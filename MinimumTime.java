bd86a52bd164e0228cba6469bf2d51691e4bb7a5
Aarsh30 <patelaarsh3011@gmail.com>
2023-03-07 21:54:35 +0530

    new change

---
+++ MinimumTime.java
@@ -0,0 +1,34 @@
+class MinimumTime{
+    public long minimumTime(int[] time, int totalTrips) {
+        long left = 1;
+		long min = time[0];
+		for(int t : time){
+			min = Math.min(min, t);
+		}
+		long right = totalTrips * min;
+
+		while(left < right){
+			long mid = (right - left) / 2 + left;
+
+			
+			long total = totalTripInGivenTime(time, mid);
+			
+			if(total < totalTrips){
+				left = mid + 1;
+			}else{
+				
+				right = mid;
+			}
+		}
+
+		return left;
+	}
+
+	private long totalTripInGivenTime(int[] time, long givenTime){
+		long totalTrips = 0l;
+		for(int tripTime : time){
+			totalTrips += ((long)givenTime) / tripTime;
+		}
+		return totalTrips;
+	}
+    }
