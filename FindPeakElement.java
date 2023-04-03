1fe28ede8d7f7cc47575a7c4fc8665e0b33807a2
Aarsh30 <patelaarsh3011@gmail.com>
2023-04-03 18:05:21 +0530

    medium

---
+++ FindPeakElement.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        
+            int low=0;
+            int high = nums.length-1;
+            int mid=0;
+            while(low<high)
+            {
+               mid = low + (high-low)/2;
+
+                if(nums[mid]<nums[mid+1])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+        return low;
+    }
+}
