15f9b6a1b06a0f382448464648ffdfbd35ae8cd9
Aarsh30 <patelaarsh3011@gmail.com>
2023-04-03 22:15:49 +0530

    medium

---
+++ SingleElementinaSortedArray.java
@@ -0,0 +1,20 @@
+class SingleElementinaSortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        int low=0;
+        int high = nums.length-2;
+
+        while(low<=high)
+        {
+            int mid = (high+low)/2;
+            if(nums[mid]==nums[mid^1])
+            {
+                low= mid+1;
+            }
+            else
+            {
+                high= mid-1;
+            }
+        }
+        return nums[low];
+    }
+}
