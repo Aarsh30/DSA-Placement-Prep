cfdb0f087375505676633e26a8db682f056837ab
Aarsh30 <patelaarsh3011@gmail.com>
2023-04-09 21:40:03 +0530

    Hard

---
+++ LargestColorValueinaDirectedGraph.java
@@ -0,0 +1,48 @@
+class LargestColorValueinaDirectedGraph {
+    public int largestPathValue(String colors, int[][] edges) {
+        int n = colors.length();
+        Map<Integer,List<Integer>> adj = new HashMap<>();
+        int[] indegree = new int[n];
+
+        for(int[] edge: edges)
+        {
+            adj.computeIfAbsent(edge[0],k-> new ArrayList<Integer>()).add(edge[1]);
+            indegree[edge[1]]++;
+        }
+        int[][] count = new int[n][26];
+        Queue<Integer> q  = new LinkedList<>();
+
+        for(int i=0;i<n;i++)
+        {
+        if(indegree[i]==0)
+        {
+            q.offer(i);
+        }
+        }
+        int answer =1,nodeSeen=0;
+        while(!q.isEmpty())
+        {
+            int node = q.poll();
+            answer= Math.max(answer,++count[node][colors.charAt(node)-'a']);
+            nodeSeen++;
+
+            if(!adj.containsKey(node))
+            {
+                continue;
+            }
+            for(int neighbor:adj.get(node))
+            {
+                for(int i=0;i<26;i++)
+                {
+                    count[neighbor][i]=Math.max(count[neighbor][i],count[node][i]);
+                }
+                indegree[neighbor]--;
+                if(indegree[neighbor]==0)
+                {
+                    q.offer(neighbor);
+                }
+            }
+        }
+        return nodeSeen < n? -1:answer;
+    }
+}
