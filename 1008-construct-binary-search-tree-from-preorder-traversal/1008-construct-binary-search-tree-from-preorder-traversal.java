20b8de2908ce8b0976630612cda96809b31e1d8a
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-07-14 12:35:04 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (48.25%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,34 +14,51 @@
  * }
  */
 class Solution {
-    public TreeNode bstFromPreorder(int[] preorder) {
-      TreeNode root = new TreeNode(preorder[0]);
-        for(int i :  preorder)
-        {
-            build(i,root);
-        }
+//     public TreeNode bstFromPreorder(int[] preorder) {
+//       TreeNode root = new TreeNode(preorder[0]);
+//         for(int i :  preorder)
+//         {
+//             build(i,root);
+//         }
         
-        return root;
+//         return root;
         
-    }
+//     }
     
-    public TreeNode build(int i,TreeNode root)
-    {
-        if(root == null)
-        {
-            root = new TreeNode(i);
-            return root;
-        }
-        if(i<root.val)
-        {
-            root.left = build(i,root.left);
+//     public TreeNode build(int i,TreeNode root)
+//     {
+//         if(root == null)
+//         {
+//             root = new TreeNode(i);
+//             return root;
+//         }
+//         if(i<root.val)
+//         {
+//             root.left = build(i,root.left);
             
-        }
-        if(i>root.val)
-        {
-            root.right = build(i,root.right);
-        }
+//         }
+//         if(i>root.val)
+//         {
+//             root.right = build(i,root.right);
+//         }
         
+//         return root;
+//     }
+// }
+    
+public TreeNode bstFromPreorder(int[]a)
+{
+    return bstfromPreorder(a,Integer.MAX_VALUE,new int[] {0});
+}
+    
+    public TreeNode bstfromPreorder(int[]a, int bound, int[]i)
+    {
+        if(i[0]==a.length ||a[i[0]]>bound)
+            return null;
+        
+        TreeNode root = new TreeNode(a[i[0]++]);
+        root.left = bstfromPreorder(a,root.val,i);
+        root.right = bstfromPreorder(a,bound,i);
         return root;
     }
 }
