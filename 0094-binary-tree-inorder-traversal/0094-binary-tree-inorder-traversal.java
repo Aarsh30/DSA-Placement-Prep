acb13db11547a566d38373344d13f6475a34346d
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-07-15 10:53:33 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (28.28%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,20 +15,86 @@
  */
 class Solution {
 
-    public static void tree(TreeNode root, List<Integer> list)
-    {
-        if(root == null)
-            return;
-       tree(root.left,list);
-       list.add(root.val);
-       tree(root.right,list);
+//     public static void tree(TreeNode root, List<Integer> list)
+//     {
+//         if(root == null)
+//             return;
+//        tree(root.left,list);
+//        list.add(root.val);
+//        tree(root.right,list);
 
-    }
+//     }
 
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        tree(root,list);
-        return list;
-    }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList<>();
+//         tree(root,list);
+//         return list;
+//     }
+// }
+    
+//     public static void tree(TreeNode root, List<Integer> list)
+//     {
+        
+//         if(root == null) return;
+       
+//         Queue<TreeNode> q = new LinkedList<TreeNode>();
+//         q.add(root);
+//         while(q.isEmpty()==false)
+//         {
+//             int count = q.size();
+//             for(int i=0;i<count;i++)
+//             {TreeNode curr = q.poll();
+//             if(i==0)
+//              list.add(curr.val);
+//             if(curr.left !=null)
+//                     q.add(curr.left);
+//              if(curr.right != null)
+//                  q.add(curr.right);
+            
+//             }
+//         }
+        
+//     }
+    
+//    public List<Integer> inorderTraversal(TreeNode root)
+//    {
+//        List<Integer> list = new ArrayList<>();
+//        tree(root,list);
+//        return list;
+//    }
+// }
+ public List<Integer> inorderTraversal(TreeNode root)
+ {
+     List<Integer> inorder = new ArrayList<>();
+     TreeNode curr = root;
+     while(curr!=null)
+     {
+         if(curr.left == null)
+         {
+             inorder.add(curr.val);
+             curr= curr.right;
+         }
+         else
+         {
+             TreeNode prev = curr.left;
+             while(prev.right!=null && prev.right!=curr)
+             {
+                 prev = prev.right;
+             }
+             if(prev.right== null)
+             { prev.right= curr;
+              curr= curr.left;
+              }
+             else
+             {
+                 prev.right = null;
+                inorder.add(curr.val);
+                 curr= curr.right;
+             }
+         }
+        
+     }
+      return inorder;
+ }
 }
