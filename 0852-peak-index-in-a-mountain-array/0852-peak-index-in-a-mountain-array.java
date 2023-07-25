f91650218f62037d40755830d83af68e66835aa2
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-07-25 20:02:08 +0530

    Time: 1 ms (15.54%), Space: 55.6 MB (36.55%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int i=0;
+        while(arr[i]<arr[i+1])
+        {
+            i++;
+        }
+        return i;
+    }
+}
