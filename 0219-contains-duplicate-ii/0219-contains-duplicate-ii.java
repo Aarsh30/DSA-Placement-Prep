6ae2316d933c580ae6b24175d9fcee336d65d8f8
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-07-26 23:47:12 +0530

    Time: 24 ms (18.50%), Space: 55.7 MB (71.17%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -12,19 +12,39 @@
 // }
 // }
 
-class Solution
-{
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-	Set<Integer> set = new HashSet<Integer>();
+// class Solution
+// {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+// 	Set<Integer> set = new HashSet<Integer>();
 	
-	for(int i = 0; i < nums.length; i++) {
-		if (i > k) 
-			set.remove(nums[i-k-1]); 
+// 	for(int i = 0; i < nums.length; i++) {
+// 		if (i > k) 
+// 			set.remove(nums[i-k-1]); 
 		
-		if (!set.add(nums[i])) 
-			return true; 
-	}
+// 		if (!set.add(nums[i])) 
+// 			return true; 
+// 	}
 	
-	return false;
-}
+// 	return false;
+// }
+// }
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            // Check if the current element already exists in the set
+            if (set.contains(nums[i]))
+                return true;
+
+            set.add(nums[i]);
+
+            // Maintain the window of size k
+            if (set.size() > k)
+                set.remove(nums[i - k]);
+        }
+
+        return false;
+    }
 }
