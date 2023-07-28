be1fbcb55b84bcb85d93869c945ab05cfdf814b1
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-07-28 15:10:58 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (93.59%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,19 +1,36 @@
 class Solution {
-    public int totalMoney(int n) {
-        int sum=0;
-        int count=1;
-        int tcount=2;
+//     public int totalMoney(int n) {
+//         int sum=0;
+//         int count=1;
+//         int tcount=2;
         
-        for(int i=1;i<=n;i++)
-        {
-            sum+=count;
-            count++;
+//         for(int i=1;i<=n;i++)
+//         {
+//             sum+=count;
+//             count++;
             
-            if(i%7==0)
-            {
-                count = tcount;
-                tcount++;
-            }    
+//             if(i%7==0)
+//             {
+//                 count = tcount;
+//                 tcount++;
+//             }    
+//         }
+        
+//         return sum;
+//     }
+    
+    public int totalMoney(int n)
+    {
+        int m = n/7;
+         int sum=0;
+        for(int i=1;i<=m;i++)
+        {
+            sum +=7*(i+3); 
+        }
+        
+        for(int i=7*m;i<n;i++)
+        {
+            sum += ++m;
         }
         
         return sum;
