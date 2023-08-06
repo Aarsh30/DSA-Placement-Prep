8c9a82415ca4610afe9dc1e7b52e669a87061d9a
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-08-06 11:51:19 +0530

    Time: 1 ms (8.14%), Space: 39.2 MB (69.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,27 @@
 class Solution {
     
-    public int uniquePathsCount(int i, int j, int [][]dp)
+    public int uniquePathsCount(int m, int n, int [][]dp)
     {
-        if(i==0 && j==0)
+        for(int  i=0;i<m;i++)
         {
-            return 1;
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                    
+                }
+                int up=0,left =0;
+                if(i>0)
+                    up = dp[i-1][j];
+                if(j>0)
+                    left = dp[i][j-1];
+                
+               dp[i][j]= up +left;
+            }
         }
-         if(i<0 || j<0)
-         {
-             return 0;
-        
-         }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int up = uniquePathsCount(i-1,j,dp);
-        int left = uniquePathsCount(i,j-1,dp);
-        
-        return dp[i][j]=up+left;
+        return dp[m-1][n-1];
     }
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
@@ -26,6 +29,6 @@ class Solution {
             {
                 Arrays.fill(rows,-1);                
             }
-        return uniquePathsCount(m-1,n-1,dp);
+        return uniquePathsCount(m,n,dp);
     }
 }
