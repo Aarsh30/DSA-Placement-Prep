510b526a2569996ab57b46a08d1a64b595316be3
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-08-16 12:10:17 +0530

    Time: 1 ms (89.14%), Space: 44.7 MB (20.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,29 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        
+   public static int trap(int arr[])
+    {
+        int n = arr.length;
+        int prefix[]= new int[n];
+        int suffix[]= new int[n];
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            prefix[i]=Math.max(prefix[i-1],arr[i]);
+
+        }
+        suffix[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            suffix[i]=Math.max(suffix[i+1],arr[i]);
+        }
         int waterTrapped =0;
         for(int i=0;i<n;i++)
         {
-            int j=i;
-            int leftMax=0,rightMax=0;
-            while(j>=0)
-            {
-                leftMax =Math.max(leftMax,height[j]);
-                j--;
-            }
-            j=i;
-            while(j<n)
-            {
-                rightMax= Math.max(rightMax,height[j]);
-                j++;
-            }
-            waterTrapped +=Math.min(rightMax,leftMax)-height[i];
+            waterTrapped+= Math.min(prefix[i],suffix[i])-arr[i];
         }
+
         return waterTrapped;
+
     }
+
+
 }
