c6b76b7f4ebf06c60172d8c018eadbdc871eae3a
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-09-25 23:22:37 +0530

    Time: 1 ms (100%), Space: 40.6 MB (71.69%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int num=0;
+
+        for(int ch:t.toCharArray())
+        {
+            num+=ch;
+        }
+        for(int ch:s.toCharArray())
+        {
+            num-=ch;
+        }
+        return (char)num;
+    }
+}
