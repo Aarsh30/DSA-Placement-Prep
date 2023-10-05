4d4de56c4b937338c7972b75cb86361d6b34e71e
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-10-05 22:53:30 +0530

    Time: 1432 ms (5.91%), Space: 46.4 MB (71.66%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,46 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-
-            int num1=-1,num2=-1,c1=0,c2=0,len=nums.length;
-
-        for(int i=0;i<len;i++)
-        {
-                if(nums[i]==num1)
-                {c1++;}
-                else if(nums[i]==num2)
-                {c2++; }
-                else if(c1==0)
-                {
-                    num1=nums[i];
-                    c1=1;
-                }
-                else if(c2==0)
-                {
-                    num2=nums[i];
-                    c2=1;
-                }
-                else{
-                c1--;
-                c2--;
-                }
-
-        }
-        List<Integer> ans = new ArrayList<Integer>();
-        c1=0;
-        c2=0;
-        for(int i=0;i<len;i++)
-        {   
-            if(nums[i]==num1)
-              { c1++;}
-              else if(nums[i]==num2)
-              {c2++;}
-        }
-              if(c1>len/3)
-              {ans.add(num1);}
-               if(c2>len/3)
-              {ans.add(num2);}
+        int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
         
-return ans;
-
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                    cnt++;
+                }
+            }
+            
+            if (cnt > n / 3 && !ans.contains(nums[i])) {
+                ans.add(nums[i]);
+            }
+        }
+        
+        return ans;
     }
 }
