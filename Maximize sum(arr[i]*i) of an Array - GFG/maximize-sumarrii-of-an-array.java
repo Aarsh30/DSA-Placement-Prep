5886c3a4abc6041496b4b450f2071ab61a38551f
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-10-06 10:59:59 +0530

    Added solution - LeetHub

---
+++ Maximize sum(arr[i]*i) of an Array - GFG/maximize-sumarrii-of-an-array.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
+            }
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.Maximize(a,n));
+        }
+        
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    final int mod = 1000000007;
+
+    int Maximize(int arr[], int n) {
+        Arrays.sort(arr);
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            long product = (long) arr[i] * i; // Calculate the product as a long
+            sum += product % mod;
+        }
+
+        return (int) (sum % mod);
+    }
+}
+
+
