8374440f1476fca6c8d09ee458e757a67d2f91f8
Aarsh Patel <105485872+Aarsh30@users.noreply.github.com>
2023-10-06 11:26:11 +0530

    Added solution - LeetHub

---
+++ Prime Number - GFG/prime-number.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isPrime(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int isPrime(int n){
+    if(n==1)
+        return 0;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+    }
+}
